8a9860f8ccb525eb1e34f77fd8cd1df231fe32b4
BUI NGOC SON <bnson@live.com>
2016-02-03 23:04:37 +0700

    Update vnlives_003.js

--- vnlives_003.js
+++ vnlives_003.js
@@ -51,7 +51,7 @@ function mycallback(json) {
 			item = item01 + item02 + item03 + item05;
 		} else {
 			var item01 = "<div class='row-fluid'><div class='span12 item'>"
-				var item02 = "<div class='clearfix'><img class='pull-left' src='" + imageThumb + " style='margin-right: 5px;' />";
+				var item02 = "<div class='clearfix'><img class='pull-left' src='" + imageThumb + "' style='margin-right: 5px;' />";
 				var item03 = "<a class='item-title' href='" + postUrl + "'>" + postTitle + "</a></div>"
 			var item05 = "</div></div>";				
 			
